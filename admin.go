dc372957004d208df47759b3b6c5b9c282091890
Quoc-Viet Nguyen <afelion@gmail.com>
2015-01-29 18:26:36 +1000

    Combine admin files

--- admin.go
+++ admin.go
@@ -6,6 +6,7 @@ package gows
 import (
 	"fmt"
 	"net/http"
+	"runtime"
 
 	"github.com/goburrow/gol"
 	"github.com/goburrow/health"
@@ -99,9 +100,82 @@ func (handler *DefaultAdminHandler) ServeHTTP(w http.ResponseWriter, r *http.Req
 	fmt.Fprintf(w, adminHTML, handler.contextPath, metricsUri, pingUri, runtimeUri, healthCheckUri)
 }
 
+// DefaultHealthCheckHandler is the http handler for /healthcheck page
+type DefaultHealthCheckHandler struct {
+	registry health.Registry
+}
+
+// NewHealthCheckHandler allocates and returns a new DefaultHealthCheckHandler
+func NewHealthCheckHandler(registry health.Registry) *DefaultHealthCheckHandler {
+	return &DefaultHealthCheckHandler{
+		registry: registry,
+	}
+}
+
+func (handler *DefaultHealthCheckHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "must-revalidate,no-cache,no-store")
+	w.Header().Set("Content-Type", "text/plain")
+
+	results := handler.registry.RunHealthChecks()
+
+	if len(results) == 0 {
+		w.WriteHeader(http.StatusNotImplemented)
+		w.Write([]byte("No health checks registered."))
+		return
+	}
+	if !isAllHealthy(results) {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	for name, result := range results {
+		fmt.Fprintf(w, "%s:\n\tHealthy: %t\n", name, result.Healthy)
+		if result.Message != "" {
+			fmt.Fprintf(w, "\tMessage: %s\n", result.Message)
+		}
+		if result.Cause != nil {
+			fmt.Fprintf(w, "\tCause: %+v\n", result.Cause)
+		}
+	}
+}
+
+// isAllHealthy checks if all are healthy
+func isAllHealthy(results map[string]*health.Result) bool {
+	for _, result := range results {
+		if !result.Healthy {
+			return false
+		}
+	}
+	return true
+}
+
 // handleAdminPing handles ping request to admin /ping
 func handleAdminPing(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "must-revalidate,no-cache,no-store")
 	w.Header().Set("Content-Type", "text/plain")
 	w.Write([]byte("pong\n"))
+}
+
+// handleAdminRuntime displays runtime statistics.
+func handleAdminRuntime(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Cache-Control", "must-revalidate,no-cache,no-store")
+	w.Header().Set("Content-Type", "text/plain")
+
+	fmt.Fprintf(w, "NumCPU: %d\nNumCgoCall: %d\nNumGoroutine: %d\n",
+		runtime.NumCPU(), runtime.NumCgoCall(), runtime.NumGoroutine())
+
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	// General statistics
+	fmt.Fprintf(w, "MemStats:\n\tAlloc: %d\n\tTotalAlloc: %d\n\tSys: %d\n\tLookups: %d\n\tMallocs: %d\n\tFrees: %d\n",
+		m.Alloc, m.TotalAlloc, m.Sys, m.Lookups, m.Mallocs, m.Frees)
+	// Main allocation heap statistics
+	fmt.Fprintf(w, "\tHeapAlloc: %d\n\tHeapSys: %d\n\tHeapIdle: %d\n\tHeapInuse: %d\n\tHeapReleased: %d\n\tHeapObjects: %d\n",
+		m.HeapAlloc, m.HeapSys, m.HeapIdle, m.HeapInuse, m.HeapReleased, m.HeapObjects)
+	// Low-level fixed-size structure allocator statistics
+	fmt.Fprintf(w, "\tStackInuse: %d\n\tStackSys: %d\n\tMSpanInuse: %d\n\tMSpanSys: %d\n\tMCacheInuse: %d\n\tMCacheSys: %d\n\tBuckHashSys: %d\n\tGCSys: %d\n\tOtherSys: %d\n",
+		m.StackInuse, m.StackSys, m.MSpanInuse, m.MSpanSys, m.MCacheInuse, m.MCacheSys, m.BuckHashSys, m.GCSys, m.OtherSys)
+	// Garbage collector statistics
+	fmt.Fprintf(w, "\tNextGC: %d\n\tLastGC: %d\n\tPauseTotalNs: %d\n\tNumGC: %d\n\tEnableGC: %t\n\tDebugGC: %t\n",
+		m.NextGC, m.LastGC, m.PauseTotalNs, m.NumGC, m.EnableGC, m.DebugGC)
+
+	fmt.Fprintf(w, "Version: %s\n", runtime.Version())
 }
