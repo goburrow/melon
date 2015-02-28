896cab8f76ea6600fa063e825064a4b24ca6fcc7
Quoc-Viet Nguyen <afelion@gmail.com>
2015-02-28 12:31:38 +1000

    Helloworld example

---
+++ example/helloworld/helloworld.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/goburrow/gomelon"
+	"github.com/goburrow/gomelon/core"
+)
+
+// resousce is the HTTP handler for homepage.
+type resource struct {
+}
+
+func (*resource) Method() string {
+	return "GET"
+}
+
+func (*resource) Path() string {
+	return "/"
+}
+
+func (*resource) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello world"))
+}
+
+// application adds resources into the application.
+type application struct {
+	gomelon.Application
+}
+
+func (app *application) Run(conf interface{}, env *core.Environment) error {
+	if err := app.Application.Run(conf, env); err != nil {
+		return err
+	}
+	env.Server.Register(&resource{})
+	return nil
+}
+
+// Build application and run with command:
+//   helloworld server path/to/config.yaml
+// Then open in browser:
+//   http://localhost:8080/application/
+//   http://localhost:8080/admin/
+func main() {
+	app := &application{}
+	gomelon.Run(app, os.Args[1:])
+}
