66bbdb40241fbcc761fd40bdaf34a7f58b905732
Quoc-Viet Nguyen <afelion@gmail.com>
2015-03-04 17:00:59 +1000

    Access log

---
+++ server/accesslog/accesslog.go
@@ -0,0 +1,121 @@
+/*
+Package accesslog provides logging for HTTP requests.
+*/
+package accesslog
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/goburrow/gomelon/server/filter"
+)
+
+const (
+	timeFormat = "02/Jan/2006:15:04:05 -0700"
+
+	xRequestID    = "X-Request-Id"
+	xForwardedFor = "X-Forwarded-For"
+)
+
+// For testing
+var now = time.Now
+
+type Filter struct {
+	writer io.Writer
+}
+
+var _ filter.Filter = (*Filter)(nil)
+
+func NewFilter(writer io.Writer) *Filter {
+	return &Filter{writer: writer}
+}
+
+func (f *Filter) Name() string {
+	return "requestlog"
+}
+
+func (f *Filter) ServeHTTP(w http.ResponseWriter, r *http.Request, chain []filter.Filter) {
+	responseWriter := &responseWriter{writer: w, status: 200}
+
+	start := now()
+	chain[0].ServeHTTP(responseWriter, r, chain[1:])
+	end := now()
+
+	remoteAddr := getRemoteAddr(r)
+	referer := r.Referer()
+	if referer == "" {
+		referer = "-"
+	}
+	userAgent := r.UserAgent()
+	if userAgent == "" {
+		userAgent = "-"
+	}
+	startTime := start.Format(timeFormat)
+	responseTime := end.Sub(start).Nanoseconds() / int64(time.Millisecond)
+	requestID := r.Header.Get(xRequestID)
+
+	// Common log format
+	fmt.Fprintf(f.writer,
+		"%s %s %s [%s] \"%s %s %s\" %d %d %q %q %d %q\n",
+		remoteAddr,
+		"-", // Identity is not supported.
+		"-", // UserID is not supported.
+		startTime,
+		r.Method,
+		r.RequestURI,
+		r.Proto,
+		responseWriter.status,
+		responseWriter.size,
+		referer,
+		userAgent,
+		responseTime,
+		requestID,
+	)
+}
+
+func getRemoteAddr(r *http.Request) string {
+	if s := r.Header.Get(xForwardedFor); s != "" {
+		return s
+	}
+	if idx := strings.LastIndex(r.RemoteAddr, ":"); idx != -1 {
+		return r.RemoteAddr[:idx]
+	}
+	return r.RemoteAddr
+}
+
+// responseWriter is a wrapper for http.ResponseWriter and store response status.
+type responseWriter struct {
+	writer http.ResponseWriter
+	status int
+	size   uint64
+}
+
+func (w *responseWriter) Header() http.Header {
+	return w.writer.Header()
+}
+
+func (w *responseWriter) Write(b []byte) (int, error) {
+	n, err := w.writer.Write(b)
+	if err == nil {
+		w.size += uint64(n)
+	}
+	return n, err
+}
+
+func (w *responseWriter) WriteHeader(status int) {
+	w.status = status
+	w.writer.WriteHeader(status)
+}
+
+func (w *responseWriter) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	if hijacker, ok := w.writer.(http.Hijacker); ok {
+		return hijacker.Hijack()
+	}
+	return nil, nil, errors.New("accesslog: http.Hijack is not implemented")
+}
---
+++ server/accesslog/accesslog_test.go
@@ -0,0 +1,99 @@
+package accesslog
+
+import (
+	"bytes"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/goburrow/gomelon/server/filter"
+)
+
+var today = time.Date(2015, time.January, 14, 1, 2, 3, 789000000, time.FixedZone("Asia/Ho_Chi_Minh", 7*60*60))
+
+func init() {
+	now = func() time.Time {
+		return today
+	}
+}
+
+func TestResponseOK(t *testing.T) {
+	var buf bytes.Buffer
+
+	builder := filter.NewChain()
+	builder.Add(NewFilter(&buf))
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	}
+
+	chain := builder.Build(http.HandlerFunc(handler))
+
+	server := httptest.NewServer(chain)
+	defer server.Close()
+
+	req, err := http.NewRequest("GET", server.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(content) != "ok" {
+		t.Fatal("unexpected response %s", content)
+	}
+	expected := `127.0.0.1 - - [14/Jan/2015:01:02:03 +0700] "GET / HTTP/1.1" 200 2 "-" "-" 0 ""` + "\n"
+	if expected != buf.String() {
+		t.Fatalf("unexpected access log %v", buf.String())
+	}
+}
+
+func TestResponseError(t *testing.T) {
+	var buf bytes.Buffer
+
+	builder := filter.NewChain()
+	builder.Add(NewFilter(&buf))
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("bad request"))
+	}
+
+	chain := builder.Build(http.HandlerFunc(handler))
+
+	server := httptest.NewServer(chain)
+	defer server.Close()
+
+	req, err := http.NewRequest("POST", server.URL+"/test", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "gomelon/1.0")
+	req.Header.Set("Referer", "test")
+	req.Header.Set("X-Request-Id", "go123")
+	req.Header.Set("X-Forwarded-For", "4.3.2.1")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	content, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if string(content) != "bad request" {
+		t.Fatal("unexpected response %s", content)
+	}
+	expected := `4.3.2.1 - - [14/Jan/2015:01:02:03 +0700] "POST /test HTTP/1.1" 400 11 "test" "gomelon/1.0" 0 "go123"` + "\n"
+	if expected != buf.String() {
+		t.Fatalf("unexpected access log %v", buf.String())
+	}
+}
